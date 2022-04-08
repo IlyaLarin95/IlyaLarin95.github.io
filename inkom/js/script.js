d8b7f6d3ab2ea7fc8c7de37c85b7ed5489964c08
Ilya <91022778+IlyaLarin95@users.noreply.github.com>
2022-04-08 04:55:24 +0300

    Update script.js

--- inkom/js/script.js
+++ inkom/js/script.js
@@ -3,3 +3,8 @@ let phoneMask = IMask(
     document.getElementById('phone-mask'), {
       mask: '+{7} (000) 000-00-00'
     });
+
+let phoneMaskModal = IMask(
+    document.getElementById('phone-mask-modal'), {
+      mask: '+{7} (000) 000-00-00'
+    });
