3fad85b1e3e5e46804e21e76de13e84911bae6a2
Ilya <91022778+IlyaLarin95@users.noreply.github.com>
2022-05-27 20:03:19 +0300

    Add files via upload

---
+++ dist/js/swiper.js
@@ -0,0 +1,20 @@
+{/* <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>; */}
+
+{/* <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>; */}
+
+
+const swiper1 = new Swiper('.swiper-1', {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    mousewheel: true,
+    spaceBetween: 23,
+  });
+
+const swiper2 = new Swiper('.swiper-2', {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    mousewheel: true,
+    spaceBetween: 23,
+  });
